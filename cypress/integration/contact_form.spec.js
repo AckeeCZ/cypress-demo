d15069c2e2ad9c51c44151ad6ee84e2eb07d2dbb
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2020-08-31 16:25:57 +0200

    ✅ Add contact form test

--- cypress/integration/contact_form.spec.js
+++ cypress/integration/contact_form.spec.js
@@ -4,4 +4,26 @@ describe('Contact form', () => {
 
         cy.get('main[data-testid="news"] ul li:last-child button').click()
     })
+
+    it('fills and submits the contact form', () => {
+        cy.visit('/cz/kontakt?hideOnboarding')
+
+        // Select box
+        cy.get('.select__control').click()
+        cy.get('.select__option:nth-child(2)').click()
+
+        // Inputs
+        cy.get('input[name="name"]').type('John Doe')
+        cy.get('input[name="email"]').type('john@gmail.com')
+        cy.get(
+            'textarea[name="text"]'
+        ).type('Pls enable progressive web app features', { delay: 2 })
+        cy.get('input[name="termsAndConditions"]').check()
+
+        // Button and submit
+        cy.get('button[type="submit"]').click()
+
+        // Assert success toast
+        cy.get('.Toastify__toast--success').should('exist')
+    })
 })
