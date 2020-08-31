cac41a58f006cea66d5b885ae76baa32e09a6b05
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2020-08-31 16:51:01 +0200

    ✅ Update test with within command

--- cypress/integration/contact_form.spec.js
+++ cypress/integration/contact_form.spec.js
@@ -5,25 +5,31 @@ describe('Contact form', () => {
         cy.get('main[data-testid="news"] ul li:last-child button').click()
     })
 
-    it('fills and submits the contact form', () => {
+    it.only('fills and submits the contact form', () => {
         cy.visit('/cz/kontakt?hideOnboarding')
 
-        // Select box
-        cy.get('.select__control').click()
-        cy.get('.select__option:nth-child(2)').click()
+        cy.get('form').within((el) => {
+            // Select box
+            cy.get('.select__control').click()
+            cy.get('.select__option:nth-child(2)').click()
 
-        // Inputs
-        cy.get('input[name="name"]').type('John Doe')
-        cy.get('input[name="email"]').type('john@gmail.com')
-        cy.get(
-            'textarea[name="text"]'
-        ).type('Pls enable progressive web app features', { delay: 2 })
-        cy.get('input[name="termsAndConditions"]').check()
+            // Inputs
+            cy.get('input[name="name"]').type('John Doe')
+            cy.get('input[name="email"]').type('john@gmail.com')
+            cy.get(
+                'textarea[name="text"]'
+            ).type('Pls enable progressive web app features', { delay: 2 })
+            cy.get('input[name="termsAndConditions"]').check()
 
-        // Button and submit
-        cy.get('button[type="submit"]').click()
+            // Button and submit
+            cy.get('button[type="submit"]').click()
+        })
 
         // Assert success toast
+        cy.get('.Toastify__toast--success').should('exist')
+
+        // Autohides
+        cy.wait(6000)
         cy.get('.Toastify__toast--success').should('exist')
     })
 })
